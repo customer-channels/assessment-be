a5dc1797db965ae7362758866ee30ebd1a691dc4
Fernando <fernandomorenoruiz@gmail.com>
2022-04-08 16:14:29 +0200

    added comments.

--- src/main/java/com/ista/isp/assessment/todo/controller/ItemController.java
+++ src/main/java/com/ista/isp/assessment/todo/controller/ItemController.java
@@ -31,22 +31,33 @@ public class ItemController {
 		this.itemService = itemService;
 	}
 	
+	/**
+	 * It creates a new Item in Todo list on database
+	 * */
 	@PostMapping
 	public ResponseEntity<Item> createItem(@RequestBody final Item item){
 		return new ResponseEntity<Item>(itemService.saveItem(item), HttpStatus.CREATED);
 	}
 	
+	/**
+	 * It modifies a existing Item of Todo list
+	 * */
 	@PutMapping("{id}")
 	public ResponseEntity<Item> updateItem(@RequestBody Item item,@PathVariable long id){
 		return new ResponseEntity<Item>(itemService.updateItem(item,id),HttpStatus.OK);
 	}
 	
-	
+	/**
+	 * It gets Item of Todo list by Id
+	 * */
 	@GetMapping("{id}")
 	public ResponseEntity<Item> getItem(@PathVariable(value = "id") long id){
 		return new ResponseEntity<Item>(itemService.getItem(id),HttpStatus.OK);
 	}
 	
+	/**
+	 * It gets all Items from Todo list
+	 * */
 	@GetMapping
 	public ResponseEntity<List<Item>> listItems(){
 		return new ResponseEntity<List<Item>>(itemService.getListItems(),HttpStatus.OK);
